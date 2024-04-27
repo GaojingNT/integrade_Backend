0d23e50b8552f1046cac0260e635cf7aee63651b
natdnai tessapan <natdanai.tess@gmail.com>
2024-04-27 19:20:25 +0700

    PBI2 success

---
+++ integradeProject/integradeProject/src/main/java/com/example/integradeproject/entities/Task.java
@@ -0,0 +1,30 @@
+package com.example.integradeproject.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    private Integer taskId;
+    private String title ;
+    private String description ;
+    private String assignees ;
+    private String status ;
+
+    private ZonedDateTime createdOn ;
+    private ZonedDateTime updatedOn ;
+
+
+
+}
